ab83363fee098ebc4e75d8d0fcd6a82c1c835231
iDay <sudaryadi47@gmail.com>
2023-07-02 17:53:46 +0700

    menambahkan element page object - signUp

---
+++ pageObject/signUp.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class SignUp:
+    base_url = "https://www.demoblaze.com"
+    btn_signUp_linkText = "Sign up"
+    txt_username_id = "sign-username"
+    txt_password_id = "sign-password"
+    btn_signUp_xpath = '//*[@id="signInModal"]/div/div/div[3]/button[2]'
+
+    def __init__(self, driver) -> None:
+        self.driver = driver()
+    
+    def open_url(self, base_url):
+        self.driver.find_element(By.LINK_TEXT, self.btn_signUp_linkText).click()
+    
+    def input_username(self, username):
+        self.driver.find_element(By.ID, self.txt_username_id).send_keys(username)
+    
+    def input_password(self, password):
+        self.driver.find_element(By.ID, self.txt_password_id).send_keys(password)
+    
+    def click_signUp(self):
+        self.driver.find_element(By.XPATH, self.btn_signUp_xpath).click()
